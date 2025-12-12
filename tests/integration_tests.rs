88ca8d2e88405a0bf6c8630d0bd5a1945839b0c5
Milosz Brzechczyn <miloszbrzechczyn@gmail.com>
2025-12-12 14:16:33 +0100

    rework: prepare new integration tests environment

---
+++ tests/integration_tests.rs
@@ -0,0 +1,29 @@
+mod common;
+mod schemas;
+mod logs;
+
+mod health {
+    use crate::common::test_app::setup_test_app;
+    use reqwest::StatusCode;
+
+    #[tokio::test]
+    async fn health_check_works() {
+        let app = setup_test_app().await;
+
+        let response = app
+            .client
+            .get(format!("{}/health", app.address))
+            .header("X-Api-Key", "secret-key")
+            .send()
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body: serde_json::Value = response.json().await.expect("Failed to parse JSON");
+
+        assert_eq!(body["status"], "healthy");
+        assert_eq!(body["service"], "log-server");
+        assert!(body["timestamp"].is_string());
+    }
+}
